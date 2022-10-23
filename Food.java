b56cc464bad07763d10f9daf37c9b97d6fce2482
Richi Hnok <grenkavoit@gmail.com>
2022-10-23 16:00:58 +0300

    создали интерфейс Consumable и абстрактный класс Food

---
+++ Food.java
@@ -0,0 +1,33 @@
+public abstract class Food implements Consumable{
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+    public Food(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) // сравниваем ссылки
+            return true;
+        if(obj == null || getClass() != obj.getClass()) // проверка на принадлежность нужному классу
+            return false;
+        Food food = (Food) obj; // преобразование к нужному классу
+        return this.name.equals(food.name); // сравниваем поля объектов
+    }
+
+    @Override
+    public void consume(){}
+}
