7ece0b68f573de88a1163d63153d177735508d33
Richi Hnok <grenkavoit@gmail.com>
2022-10-24 17:35:13 +0300

    В MainApplication сделал основу, включающуу выбор команд с помощью switch, для работы консольного приложения с помощью бесконечного цикла. Реализовал базовый функционал  команд: -add, -show, -help, -exit.

--- MainApplication.java
+++ MainApplication.java
@@ -3,12 +3,39 @@ import java.util.Scanner;
 public class MainApplication {
 
     public static void main(String[] args){  
+        System.out.println("Programm started.");
         Scanner console = new Scanner(System.in);
         
         Breakfast breakfast = new Breakfast();
-        breakfast.addFood(console);
-        breakfast.addFood(console);
-        breakfast.show();
+
+        while(true){
+            System.out.print("> ");
+            String command = console.nextLine();
+
+            if(command.equals("-exit")){
+                System.out.println("Program completed.");
+                break;
+            }
+
+            switch(command){
+                case "-add":
+                    breakfast.addFood(console);
+                    break;
+                case "-show":
+                    breakfast.show();
+                    break;
+                case "-help":
+                    System.out.println("The following commands are available:");
+                    System.out.println("-add  (Adds to breakfast another sandwich with 2 fillings)");
+                    System.out.println("-show (Show from what breakfast consits)");
+                    System.out.println("-help (Show available commands and its functions)");
+                    System.out.println("-exit (Completes programm)");
+                    break;
+                default :
+                    System.out.println("There is no such command. If you struggle use -help.");
+                    break;
+            }
+        }
 
         console.close();
     }
